3010214abd1a9b915a935dc637f90c5b660f1043
Ninepxl <2290835271@qq.com>
2025-08-03 16:41:31 +0800

    资源管理器

--- Assets/Script/Frame/AssetHandle/AssetHandle.cs
+++ Assets/Script/Frame/AssetHandle/AssetHandle.cs
@@ -1,34 +1,36 @@
-using Frame;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.AddressableAssets;
 
-/// <summary>
-/// 加载Texture2D、AudioClip、Sprite 等资源Handle
-/// </summary>
-/// <typeparam name="T">资源类型</typeparam>
-public class AssetHandle<T> : AssetHandleBase
+namespace Frame
 {
-    private AsyncOperationHandle<T> m_handle;
-    public T Result;
-    public AssetHandle(AsyncOperationHandle<T> handle, object key)
+    /// <summary>
+    /// 加载Texture2D、AudioClip、Sprite 等资源Handle
+    /// </summary>
+    /// <typeparam name="T">资源类型</typeparam>
+    public class AssetHandle<T> : AssetHandleBase
     {
-        CurType = AssetType.Common;
-        m_handle = handle;
-        Key = key;
-        Success = false;
-        IsDone = false;
-    }
-
-    public override float GetProgress()
-    {
-        return m_handle.PercentComplete;
-    }
-    public override void Release()
-    {
-        // 如果资源没有被释放
-        if (m_handle.IsValid())
+        private AsyncOperationHandle<T> m_handle;
+        public T Result;
+        public AssetHandle(AsyncOperationHandle<T> handle, object key)
         {
-            Addressables.Release(Result);
+            CurType = AssetType.Common;
+            m_handle = handle;
+            Key = key;
+            Success = false;
+            IsDone = false;
+        }
+
+        public override float GetProgress()
+        {
+            return m_handle.PercentComplete;
+        }
+        public override void Release()
+        {
+            // 如果资源没有被释放
+            if (m_handle.IsValid())
+            {
+                Addressables.Release(Result);
+            }
         }
     }
 }
--- Assets/Script/Frame/AssetHandle/AssetHandleComponent.cs
+++ Assets/Script/Frame/AssetHandle/AssetHandleComponent.cs
@@ -1,13 +1,54 @@
-﻿namespace Frame
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceLocations;
+
+namespace Frame
 {
     /// <summary>
     /// AssetHandleComponent 组件相当于AssetHandle的管理器，
     /// </summary>
     public class AssetHandleComponent : GameComponent
     {
+        private Dictionary<object, AssetHandleBase> m_AssetHandleCache = new Dictionary<object, AssetHandleBase>();
         protected override void Awake()
         {
             base.Awake();
+            m_AssetHandleCache.Clear();
+        }
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            ClearAllCache();
+            Addressables.InternalIdTransformFunc = null;
+            Addressables.LoadAssetAsync<GameObject>("");
+        }
+        private void ClearAllCache()
+        {
+            foreach (var handle in m_AssetHandleCache)
+            {
+                handle.Value.Release();
+            }
+            m_AssetHandleCache.Clear();
+        }
+        public async UniTask StartupCoroutine()
+        {
+            Debug.Log("Addressables 初始化中...");
+            AsyncOperationHandle initializeHandle = Addressables.InitializeAsync(false);
+            await initializeHandle;
+            if (initializeHandle.Status == AsyncOperationStatus.Succeeded)
+            {
+                Debug.Log("Addressables 初始化 成功");
+            }
+            else
+            {
+                Debug.LogError($"Addressables 初始化 失败 {initializeHandle.OperationException}");
+            }
+
+            // 在Demo开发中，初始化句柄通常可以立即释放
+            Addressables.Release(initializeHandle);
         }
     }
 }
